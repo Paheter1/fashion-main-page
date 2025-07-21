599123d9b925310f033e9bcd9f3c3681909ce1ba
Paheter <137654319+Paheter1@users.noreply.github.com>
2025-07-21 14:28:38 +0700

    day3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
+import Header from "./components/header/Header";
 
 function App() {
 
   const project = 'Fashion';
   return (
     <div className="App">
-     <h1>{project}</h1>
-     
+     <Header/>
     </div>
   );
 }
---
+++ src/components/promo/Promo.jsx
@@ -0,0 +1,14 @@
+const Promo = () => {
+    return (
+        <section className="Promo">
+            <div className="container">
+                <div className="promo__content">
+                    <div className="promo__text"></div>
+                    <div className="promo__img"></div>
+                </div>
+            </div>
+        </section>
+    );
+}
+ 
+export default Promo;
