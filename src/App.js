1e91bf7cbac3d6aeb9bda2af7b60c06e73480de0
Paheter <137654319+Paheter1@users.noreply.github.com>
2025-07-21 15:28:05 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/header/Header";
+import Promo from "./components/promo/Promo";
 
 function App() {
 
   const project = 'Fashion';
   return (
     <div className="App">
-     <Header/>
+     <Header />
+     <Promo />
     </div>
   );
 }
--- src/components/promo/Promo.jsx
+++ src/components/promo/Promo.jsx
@@ -1,10 +1,15 @@
+import './promo.css'
+import promoImg from './../../img/images/header-img.jpg'
+
 const Promo = () => {
     return (
         <section className="Promo">
             <div className="container">
                 <div className="promo__content">
-                    <div className="promo__text"></div>
-                    <div className="promo__img"></div>
+                    <div className="promo__text">TEXT</div>
+                    <div className="promo__img">
+                        <img src={promoImg} alt="Promo" />
+                    </div>
                 </div>
             </div>
         </section>
