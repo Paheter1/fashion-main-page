e73a5c1a17ae680ea2712f28b02c553883f536e4
Paheter <137654319+Paheter1@users.noreply.github.com>
2025-07-25 11:09:44 +0700

    loadMac

--- src/components/arrivals/Arrivals.jsx
+++ src/components/arrivals/Arrivals.jsx
@@ -5,9 +5,11 @@ const Arrivals = () => {
         <section className='arrivals'>
             <div className="container">
                 <div className="arrivals__header">
-
+                    <h2 className="title-2">NEW ARRIVALS</h2>
                 </div>
-                <div className="arrivals__cards"></div>
+                <div className="arrivals__cards">
+                    
+                </div>
             </div>
         </section>
     );
